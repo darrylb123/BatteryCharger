283c1f1c2f8ccb0de702eb444c536c1341f2bdae
Darryl Bond <darryl.bond@gmail.com>
2021-08-17 19:42:27 +1000

    Added GPLv2 license header

--- BatteryCharger.ino
+++ BatteryCharger.ino
@@ -1,4 +1,18 @@
 /* Battery Charger Multiplexer using ESP8266 or ESP32*/
+/*
+Copyright (C) 2021  Darryl Bond
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 #define STATION 1
 // #define SOFTAP 1
 #define VER2 1
